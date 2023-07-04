a38a741d105e6f176fde227aa089d8d8839d0cab
Denis Quiros-Vega <albertoquive1993@gmail.com>
2023-07-04 15:54:40 -0600

    my second commit c#

---
+++ Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+class program
+
+{
+    static void Main (string [] args)
+    {
+        Console.WriteLine ("Operaciones y logicas variable Byte");
+        byte a = 75;
+        byte b = 95;
+        Console.WriteLine (a + b);
+        Console.WriteLine (a - b);
+        Console.WriteLine (a * b);
+        Console.WriteLine (a / b);
+        Console.WriteLine (a % b);
+        Console.WriteLine (a < b);
+        Console.WriteLine (a > b);
+        Console.WriteLine (a == b);
+        Console.WriteLine (a >= b);
+        Console.WriteLine (a <= b);
+        Console.WriteLine (a != b);
+
+        Console.WriteLine ("Operaciones y logicas variable short");
+        short c = 35;
+        short d = -50;
+        Console.WriteLine (c + d);
+        Console.WriteLine (c - d);
+        Console.WriteLine (c * d);
+        Console.WriteLine (c * d);
+        Console.WriteLine (c / d);
+        Console.WriteLine (c % d);
+        Console.WriteLine (c < d);
+        Console.WriteLine (c > d);
+        Console.WriteLine (c == d);
+        Console.WriteLine (c >= d);
+        Console.WriteLine (c <= d);
+        Console.WriteLine (c != d);
+
+        Console.WriteLine ("OPeraciones y logicas variable int");
+        int e = 29;
+        int f = 55;
+        Console.WriteLine (e + f);
+        Console.WriteLine (e - f);
+        Console.WriteLine (e * f);
+        Console.WriteLine (e * f);
+        Console.WriteLine (e / f);
+        Console.WriteLine (e % f);
+        Console.WriteLine (e < f);
+        Console.WriteLine (e > f);
+        Console.WriteLine (e == f);
+        Console.WriteLine (e >= f);
+        Console.WriteLine (e <= f);
+        Console.WriteLine (e != f);
+
+        Console.WriteLine ("Operaciones y logicas variable float");
+        float g = 1.533f;
+        float h = 3.755f;
+        Console.WriteLine (g + h);
+        Console.WriteLine (g - h);
+        Console.WriteLine (g * h);
+        Console.WriteLine (g * h);
+        Console.WriteLine (g / h);
+        Console.WriteLine (g % h);
+        Console.WriteLine (g < h);
+        Console.WriteLine (g > h);
+        Console.WriteLine (g == h);
+        Console.WriteLine (g >= h);
+        Console.WriteLine (g <= h);
+        Console.WriteLine (g != h);
+
+        Console.WriteLine ("Operaciones y logicas variable double");
+        double i = 50.50;
+        double j = 37.50;
+        Console.WriteLine (i + j);
+        Console.WriteLine (i - j);
+        Console.WriteLine (i * j);
+        Console.WriteLine (i * j);
+        Console.WriteLine (i / j);
+        Console.WriteLine (i % j);
+        Console.WriteLine (i < j);
+        Console.WriteLine (i > j);
+        Console.WriteLine (i == j);
+        Console.WriteLine (i >= j);
+        Console.WriteLine (i <= j);
+        Console.WriteLine (i != j);
+
+        Console.WriteLine ("Opraciones y logicas variable long");
+        long k = 20000000000;
+        long l = 45000000000;
+        Console.WriteLine (k + l);
+        Console.WriteLine (k - l);
+        Console.WriteLine (k * l);
+        Console.WriteLine (k * l);
+        Console.WriteLine (k / l);
+        Console.WriteLine (k % l);
+        Console.WriteLine (k < l);
+        Console.WriteLine (k > l);
+        Console.WriteLine (k == l);
+        Console.WriteLine (k >= l);
+        Console.WriteLine (k <= l);
+        Console.WriteLine (k != l);
+
+        Console.WriteLine ("Operaciones y logicas variable decimal");
+        decimal m = 2.465654561516565656446M;
+        decimal n = 1.02154587895645454895869M;
+        Console.WriteLine (m + n);
+        Console.WriteLine (m - n);
+        Console.WriteLine (m * n);
+        Console.WriteLine (m * n);
+        Console.WriteLine (m / n);
+        Console.WriteLine (m % n);
+        Console.WriteLine (m < n);
+        Console.WriteLine (m > n);
+        Console.WriteLine (m == n);
+        Console.WriteLine (m >= n);
+        Console.WriteLine (m <= n);
+        Console.WriteLine (m != n);
+        
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
